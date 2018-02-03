1882dd5fe01998372039d6f8435395cb5710c3f0
Daniel Echeverry Giraldo <dfecheverryg@unal.edu.co>
2018-02-03 05:07:20 +0000

    Done.

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,15 @@
 require 'pry'
 
 def my_all?(collection)
-
+  index = 0
+  array = []
+  while index < collection.length
+    array << yield(collection[index])
+    index += 1
+  end
+  if array.include?(false)
+    false
+  else
+    true
+  end
 end
