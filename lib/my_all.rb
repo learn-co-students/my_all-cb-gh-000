b2d8a18644cbf3572fc4289d7342f92bbd386c68
Harsh Vardhan <thvardhan.god@gmail.com>
2018-03-01 02:36:08 +0000

    Done.

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,12 @@
 require 'pry'
 
 def my_all?(collection)
-
+  if block_given?
+    i=0
+    while i<collection.size
+      return false if yield(collection[i])==!true
+      i+=1
+    end
+  end
+  return true
 end
