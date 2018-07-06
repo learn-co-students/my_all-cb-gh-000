b296f68aedf1dce37921c97487fdcc5f5523ed84
Pranav Sharma <ps20maths@gmail.com>
2018-07-06 09:58:27 +0000

    Done.

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,17 @@
 require 'pry'
 
 def my_all?(collection)
+  i = 0
+  arr = []
+  while i < collection.length
+    arr << yield(collection[i])
+    i+=1
+  end
+  if arr.include?(false)
+     false
 
-end
+ end
+  else
+    true
+   end
+ end
