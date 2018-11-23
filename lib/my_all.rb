0c1627fe4dddb381b663ee36877a6986c3a6cb48
Ji Li <liji15@mails.tsinghua.edu.cn>
2018-11-23 06:34:09 +0000

    Done.

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,15 @@
 require 'pry'
 
 def my_all?(collection)
-
+  if collection.length == 0
+    return false
+  else
+    i = 0
+    block_return_values = []
+    while i < collection.length
+      block_return_values << yield(collection[i])
+      i += 1
+    end
+    !block_return_values.include?(false)
+  end
 end
