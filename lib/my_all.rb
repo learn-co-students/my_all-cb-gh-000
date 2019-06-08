16bd8c7e958d39514a968ed307bc0df7f7f99029
Sanskar Bansal <sanskarbnsl75@gmail.com>
2019-06-08 16:09:28 +0000

    Done.

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,13 @@
 require 'pry'
 
 def my_all?(collection)
-
+  i = 0
+  block_return_values = []
+  while i < collection.length
+    if yield(collection[i]) == false
+      return false
+    end
+    i+=1
+  end
+  true
 end
